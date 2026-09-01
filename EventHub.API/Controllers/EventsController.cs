e635b36a7e24f432868b2f28343e70c43b2bdb2d
Jp <mauesjoao15@gmail.com>
2026-09-01 17:27:02 -0300

    feat: create events controller

---
+++ EventHub.API/Controllers/EventsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventsController : ControllerBase
+    {
+    }
+}
