b4da99cd96063dd5e43208520682e7fc4748ec5b
Jp <mauesjoao15@gmail.com>
2026-09-02 14:26:27 -0300

    feat: implement event creation

--- EventHub.API/Controllers/EventsController.cs
+++ EventHub.API/Controllers/EventsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EventHub.API.Data;
+using EventHub.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventHub.API.Controllers
 {
@@ -6,5 +9,43 @@ namespace EventHub.API.Controllers
     [Route("api/[controller]")]
     public class EventsController : ControllerBase
     {
+
+        private readonly AppDbContext _context;
+
+        public EventsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetAll()
+        {
+            var events = await _context.Events.ToListAsync();
+
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Event>> GetById(int id)
+        {
+            var eventItem = await _context.Events.FindAsync(id);
+
+            if(eventItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(eventItem);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Event>> Create(Event eventItem)
+        {
+            _context.Events.Add(eventItem);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = eventItem.Id }, eventItem);
+        }
     }
 }
